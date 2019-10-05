5c10cd1adb24a562740efd61e399700768da2ece
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-10-05 13:10:06 -0400

    fixed ASCII txt, format

--- simulation/_version.py
+++ simulation/_version.py
@@ -1,28 +1,32 @@
 '''
-        Created on Oct 10, 2019
-        @author: Andrew Abi-Mansour
+A module that stores metadata for PyGran.simulation package
+ 
+Created on Oct 10, 2019
+Author: Andrew Abi-Mansour
 
-        This is the
-         __________         ________
-        ██████╗ ██╗   ██╗ ██████╗ ██████╗  █████╗ ███╗   ██╗
-        ██╔══██╗╚██╗ ██╔╝██╔════╝ ██╔══██╗██╔══██╗████╗  ██║
-        ██████╔╝ ╚████╔╝ ██║  ███╗██████╔╝███████║██╔██╗ ██║
-        ██╔═══╝   ╚██╔╝  ██║   ██║██╔══██╗██╔══██║██║╚██╗██║
-        ██║        ██║   ╚██████╔╝██║  ██║██║  ██║██║ ╚████║
-        ╚═╝        ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
-                                                                                                                                                                           $        DEM simulation and analysis toolkit
-        http://www.pygran.org, support@pygran.org
+This is the::
 
-        Core developer and main author:
-        Andrew Abi-Mansour, andrew.abi.mansour@pygran.org
+  ██████╗ ██╗   ██╗ ██████╗ ██████╗  █████╗ ███╗   ██╗
+  ██╔══██╗╚██╗ ██╔╝██╔════╝ ██╔══██╗██╔══██╗████╗  ██║
+  ██████╔╝ ╚████╔╝ ██║  ███╗██████╔╝███████║██╔██╗ ██║
+  ██╔═══╝   ╚██╔╝  ██║   ██║██╔══██╗██╔══██║██║╚██╗██║
+  ██║        ██║   ╚██████╔╝██║  ██║██║  ██║██║ ╚████║
+  ╚═╝        ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
 
-        PyGran is open-source, distributed under the terms of the GNU Public
-        License, version 2 or later. It is distributed in the hope that it will
-        be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-        of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have
-        received a copy of the GNU General Public License along with PyGran.
-        If not, see http://www.gnu.org/licenses . See also top-level README
-        and LICENSE files.
+DEM simulation and analysis toolkit
+http://www.pygran.org, support@pygran.org
+
+Core developer and main author:
+Andrew Abi-Mansour, andrew.abi.mansour@pygran.org
+
+PyGran is open-source, distributed under the terms of the GNU Public
+License, version 2 or later. It is distributed in the hope that it will
+be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have
+received a copy of the GNU General Public License along with PyGran.
+If not, see http://www.gnu.org/licenses . See also top-level README
+and LICENSE files.
+
 '''
 __version__ = '1.0.0'
 __author__ = 'Andrew Abi-Mansour'
