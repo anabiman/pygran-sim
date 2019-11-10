12054df5f1abfe74ff5b58d783d5d9306e2d1952
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-11-10 13:55:31 -0500

    improved file descr

--- PyGranSim/model_template.h
+++ PyGranSim/model_template.h
@@ -1,79 +1,73 @@
 /* ----------------------------------------------------------------------
 
-  Created on November 07, 2018
-  @author: Andrew Abi-Mansour
+A C++ header template file for contact models in LIGGGHTS
 
+Created on November 07, 2018
+Author: Andrew Abi-Mansour
 
-  This is the 
-   __________         ________                     
+This is the::
+
   ██████╗ ██╗   ██╗ ██████╗ ██████╗  █████╗ ███╗   ██╗
   ██╔══██╗╚██╗ ██╔╝██╔════╝ ██╔══██╗██╔══██╗████╗  ██║
   ██████╔╝ ╚████╔╝ ██║  ███╗██████╔╝███████║██╔██╗ ██║
   ██╔═══╝   ╚██╔╝  ██║   ██║██╔══██╗██╔══██║██║╚██╗██║
   ██║        ██║   ╚██████╔╝██║  ██║██║  ██║██║ ╚████║
   ╚═╝        ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
-                                                      
-    DEM simulation and analysis toolkit
-    http://www.pygran.org, support@pygran.org
 
-    Core developer and main author:
-    Andrew Abi-Mansour, andrew.abi.mansour@pygran.org
+DEM simulation and analysis toolkit
+http://www.pygran.org, support@pygran.org
 
-    PyGran is open-source, distributed under the terms of the GNU Public
-    License, version 2 or later. It is distributed in the hope that it will
-    be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-    of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have
-    received a copy of the GNU General Public License along with PyGran.
-    If not, see http://www.gnu.org/licenses . See also top-level README
-    and LICENSE files.
+Core developer and main author
+Andrew Abi-Mansour, andrew.abi.mansour@pygran.org
 
- -------------------------------------------------------------------------
+PyGran is open-source, distributed under the terms of the GNU Public
+License, version 2 or later. It is distributed in the hope that it will
+be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have
+received a copy of the GNU General Public License along with PyGran.
+If not, see http://www.gnu.org/licenses . See also top-level README
+and LICENSE files.
 
-   A C++ header template file for contact models in LIGGGHTS
+---------------------------------------------------------------------
+This is the
 
- --------------------------------------------------------------------------
+██╗     ██╗ ██████╗  ██████╗  ██████╗ ██╗  ██╗████████╗███████╗
+██║     ██║██╔════╝ ██╔════╝ ██╔════╝ ██║  ██║╚══██╔══╝██╔════╝
+██║     ██║██║  ███╗██║  ███╗██║  ███╗███████║   ██║   ███████╗
+██║     ██║██║   ██║██║   ██║██║   ██║██╔══██║   ██║   ╚════██║
+███████╗██║╚██████╔╝╚██████╔╝╚██████╔╝██║  ██║   ██║   ███████║
+╚══════╝╚═╝ ╚═════╝  ╚═════╝  ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝®
 
+DEM simulation engine, released by
+DCS Computing Gmbh, Linz, Austria
+http://www.dcs-computing.com, office@dcs-computing.com
 
-  ----------------------------------------------------------------------
-    This is the
+LIGGGHTS® is part of CFDEM®project:
+http://www.liggghts.com | http://www.cfdem.com
 
-    ██╗     ██╗ ██████╗  ██████╗  ██████╗ ██╗  ██╗████████╗███████╗
-    ██║     ██║██╔════╝ ██╔════╝ ██╔════╝ ██║  ██║╚══██╔══╝██╔════╝
-    ██║     ██║██║  ███╗██║  ███╗██║  ███╗███████║   ██║   ███████╗
-    ██║     ██║██║   ██║██║   ██║██║   ██║██╔══██║   ██║   ╚════██║
-    ███████╗██║╚██████╔╝╚██████╔╝╚██████╔╝██║  ██║   ██║   ███████║
-    ╚══════╝╚═╝ ╚═════╝  ╚═════╝  ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝®
+Core developer and main author:
+Christoph Kloss, christoph.kloss@dcs-computing.com
 
-    DEM simulation engine, released by
-    DCS Computing Gmbh, Linz, Austria
-    http://www.dcs-computing.com, office@dcs-computing.com
+LIGGGHTS® is open-source, distributed under the terms of the GNU Public
+License, version 2 or later. It is distributed in the hope that it will
+be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have
+received a copy of the GNU General Public License along with LIGGGHTS®.
+If not, see http://www.gnu.org/licenses . See also top-level README
+and LICENSE files.
 
-    LIGGGHTS® is part of CFDEM®project:
-    http://www.liggghts.com | http://www.cfdem.com
-
-    Core developer and main author:
-    Christoph Kloss, christoph.kloss@dcs-computing.com
-
-    LIGGGHTS® is open-source, distributed under the terms of the GNU Public
-    License, version 2 or later. It is distributed in the hope that it will
-    be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-    of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have
-    received a copy of the GNU General Public License along with LIGGGHTS®.
-    If not, see http://www.gnu.org/licenses . See also top-level README
-    and LICENSE files.
-
-    LIGGGHTS® and CFDEM® are registered trade marks of DCS Computing GmbH,
-    the producer of the LIGGGHTS® software and the CFDEM®coupling software
-    See http://www.cfdem.com/terms-trademark-policy for details.
+LIGGGHTS® and CFDEM® are registered trade marks of DCS Computing GmbH,
+the producer of the LIGGGHTS® software and the CFDEM®coupling software
+See http://www.cfdem.com/terms-trademark-policy for details.
 
 -------------------------------------------------------------------------
-    Contributing author and copyright for this file:
-    Christoph Kloss (DCS Computing GmbH, Linz)
-    Christoph Kloss (JKU Linz)
-    Richard Berger (JKU Linz)
+Contributing author and copyright for this file:
+Christoph Kloss (DCS Computing GmbH, Linz)
+Christoph Kloss (JKU Linz)
+Richard Berger (JKU Linz)
 
-    Copyright 2012-     DCS Computing GmbH, Linz
-    Copyright 2009-2012 JKU Linz
+Copyright 2012-     DCS Computing GmbH, Linz
+Copyright 2009-2012 JKU Linz
 ------------------------------------------------------------------------- */
 
 #ifdef NORMAL_MODEL
