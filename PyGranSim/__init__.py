1a30d8900eaeddae160d8006c042e2a003975a6c
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-11-10 13:55:22 -0500

    improved docstrings for _findEngines

--- PyGranSim/__init__.py
+++ PyGranSim/__init__.py
@@ -1,8 +1,8 @@
 '''
-  Created on March 30, 2016
-  @author: Andrew Abi-Mansour
+Created on March 30, 2016
+Author: Andrew Abi-Mansour
 
-  This is the::
+This is the::
 
   ██████╗ ██╗   ██╗ ██████╗ ██████╗  █████╗ ███╗   ██╗
   ██╔══██╗╚██╗ ██╔╝██╔════╝ ██╔══██╗██╔══██╗████╗  ██║
@@ -10,20 +10,20 @@
   ██╔═══╝   ╚██╔╝  ██║   ██║██╔══██╗██╔══██║██║╚██╗██║
   ██║        ██║   ╚██████╔╝██║  ██║██║  ██║██║ ╚████║
   ╚═╝        ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
-                                                      
-  DEM simulation and analysis toolkit
-  http://www.pygran.org, support@pygran.org
 
-  Core developer and main author:
-  Andrew Abi-Mansour, andrew.abi.mansour@pygran.org
+DEM simulation and analysis toolkit
+http://www.pygran.org, support@pygran.org
 
-  PyGran is open-source, distributed under the terms of the GNU Public
-  License, version 2 or later. It is distributed in the hope that it will
-  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have
-  received a copy of the GNU General Public License along with PyGran.
-  If not, see http://www.gnu.org/licenses . See also top-level README
-  and LICENSE files.
+Core developer and main author
+Andrew Abi-Mansour, andrew.abi.mansour@pygran.org
+
+PyGran is open-source, distributed under the terms of the GNU Public
+License, version 2 or later. It is distributed in the hope that it will
+be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. You should have
+received a copy of the GNU General Public License along with PyGran.
+If not, see http://www.gnu.org/licenses . See also top-level README
+and LICENSE files.
 '''
 
 from .dem import *
@@ -32,12 +32,12 @@ from . import model_liggghts, generator, engine_liggghts
 from ._version import __version__, __author__, __email__
 
 class _findEngines:
-	def __init__(self):
-		# Any engine module *must* follow the naming convention: engine_foo.py
-		# If the engine is found, it will be linked via setattr to be imported
-		# in DEM.py as PyGranSim.engine_foo. The engine is set by the user
-		# as DEM.simulation.engines.foo
+	''' Any engine module *must* follow the naming convention: engine_foo.py
+	If engine `foo` is found, it will be linked via setattr to be imported
+	in DEM.py as PyGranSim.engine_foo. The engine is set by the user
+	as DEM.simulation.engines.foo. '''
 
+	def __init__(self):
 		_dir, _ = __file__.split('__init__.py')
 		pyFiles = _glob(_dir + '*.py')
 
